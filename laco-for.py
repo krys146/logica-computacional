e45d29a2b96fad03f053f35765746fceed68efea
krys146 <kristyan.nhaia@escola.pr.gov.br>
2025-09-22 12:19:56 -0300

    Create laco-for.py

---
+++ laco-for.py
@@ -0,0 +1,31 @@
+for i in range(1, 6):
+
+    print(i)
+
+ for i in range(3):
+
+    print("Estudando lógica!")
+
+for i in range(10, 0,-1):
+
+    print(i)  
+  
+  for i in range(3, 31, 6):
+
+    print(i)
+
+for i in range(1, 12, 2):
+
+    print(i)
+  
+for i in range(5):
+
+    print("Python é lógico!")
+ 
+frase = 'tenha uma boa noite!'
+
+repeticao = 9
+
+
+for i in range(repeticao):
+    print(frase)
